6b4714ddde092b703337f2588dc2b0c4e868f8d8
Abhi21puri <abhinavpurohit321@gmail.com>
2026-01-18 16:31:38 +0530

    Add implementations for various array algorithms including binary search, max subarray, and reverse array

---
+++ Maxsubarray.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Maxsubarray {
+
+    public static void maxsubarray(int number[]){
+        int currsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+         for(int i = 0; i<number.length; i++){
+            int start =i;
+            for(int j=i; j<number.length; j++){
+                int end = j;
+                currsum =0;
+
+             for(int k= start; k<=end; k++){
+                currsum += number[k];
+             }
+             System.out.println(currsum);
+             if(maxsum < currsum){
+                maxsum = currsum;
+             }
+             
+          
+          
+            }
+
+
+
+        }
+        System.out.println("max sum = " + maxsum);
+
+    }
+
+     public static void main(String args[]){
+        int number[] = {1,-2,6,-1,3};
+        maxsubarray(number);
+     }
+        
+         
+         
+ }         
+    
+
---
+++ linearsearch.java
@@ -0,0 +1,24 @@
+public class linearsearch {
+    
+    public static int linearsearch(int number[], int key){
+        for(int i = 0; i<number.length; i++){
+            if(number[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        int number[] = {2,4,6,8,10,12,14};
+        int key = 15;
+        
+        int index = linearsearch(number, key);
+        if(index == -1){
+            System.out.println("Not found");
+        } else{
+            System.out.println("key in index : " + index);
+        }
+    
+    }
+    
+}
